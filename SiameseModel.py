08a50d13eea10ad0448dd80f4fc47acc00786ad5
Luojiaji <lt920@126.com>
2019-06-03 21:40:16 +0800

    change training method

--- SiameseModel.py
+++ SiameseModel.py
@@ -8,7 +8,7 @@ from keras.callbacks import TensorBoard
 from keras.datasets import mnist
 from keras.models import Model
 from keras.layers import Input, Flatten, Dense, Dropout, Lambda
-from keras.optimizers import RMSprop
+from keras.optimizers import RMSprop, SGD
 from keras import backend as K
 from keras.models import load_model
 num_classes = 10
@@ -55,16 +55,39 @@ def create_pairs(x, digit_indices):
             labels += [1, 0]
     return np.array(pairs), np.array(labels)
 
+def create_rand_batch_pairs(x, digit_indices,batch):
+    '''Positive and negative pair creation.
+    Alternates between positive and negative pairs.
+    '''
+    pairs = []
+    labels = []
+        
+    for d in range(int(batch/2)):
+        n = np.random.randint(10)
+        ind1 = np.random.randint(len(digit_indices[n]))
+        ind2 = np.random.randint(len(digit_indices[n]))
+        z1, z2 = digit_indices[n][ind1], digit_indices[n][ind2]
+        pairs += [[x[z1], x[z2]]]
+        
+        dn = np.random.randint(10)
+        while dn == n:
+            dn = np.random.randint(10)
+            
+        ind3 = np.random.randint(len(digit_indices[dn]))
+        z1, z2 = digit_indices[n][ind1], digit_indices[dn][ind3]
+        pairs += [[x[z1], x[z2]]]
+        labels += [1, 0]    
+    return np.array(pairs), np.array(labels)
 
 def create_base_network(input_shape):
     '''Base network to be shared (eq. to feature extraction).
     '''
     input = Input(shape=input_shape)
     x = Flatten()(input)
+    x = Dense(512, activation='relu')(x)
+#    x = Dropout(0.1)(x)
     x = Dense(128, activation='relu')(x)
-    x = Dropout(0.1)(x)
-    x = Dense(128, activation='relu')(x)
-    x = Dropout(0.1)(x)
+#    x = Dropout(0.1)(x)
     x = Dense(128, activation='relu')(x)
     return Model(input, x)
 
@@ -98,11 +121,11 @@ x_test /= 255
 input_shape = x_train.shape[1:]
 
 # create training+test positive and negative pairs
-digit_indices = [np.where(y_train == i)[0] for i in range(num_classes)]
-tr_pairs, tr_y = create_pairs(x_train, digit_indices)
+train_digit_indices = [np.where(y_train == i)[0] for i in range(num_classes)]
+train_pairs, train_y = create_pairs(x_train, train_digit_indices)
 
-digit_indices = [np.where(y_test == i)[0] for i in range(num_classes)]
-te_pairs, te_y = create_pairs(x_test, digit_indices)
+test_digit_indices = [np.where(y_test == i)[0] for i in range(num_classes)]
+test_pairs, test_y = create_pairs(x_test, test_digit_indices)
 
 # network definition
 base_network = create_base_network(input_shape)
@@ -125,7 +148,8 @@ model.summary()
 
 # train
 if train == True:
-    rms = RMSprop()
+    # rms = RMSprop()
+    rms = RMSprop(lr=0.001, rho=0.9, epsilon=1e-06)
     model.compile(loss=contrastive_loss, optimizer=rms, metrics=[accuracy])
     
 #    history=model.fit([tr_pairs[:, 0], tr_pairs[:, 1]], tr_y,
@@ -150,14 +174,42 @@ if train == True:
 #    
 #    print('* Accuracy on training set: %0.2f%%' % (100 * tr_acc))
 #    print('* Accuracy on test set: %0.2f%%' % (100 * te_acc))
-    
-    for i in range(1000):
+    start = 0    
+    for it in range(50000):
+#        train_loss, train_accuracy = model.train_on_batch(
+#                [tr_pairs[:, 0], tr_pairs[:, 1]], tr_y)
+        train_pairs, train_y = create_rand_batch_pairs(x_train, train_digit_indices,256)
         train_loss, train_accuracy = model.train_on_batch(
-                [tr_pairs[:, 0], tr_pairs[:, 1]], tr_y)
-        print('iteration:',i,'loss:',train_loss,'accuracy:',train_accuracy)
-    
-elif train == False:
-    
+                [train_pairs[:, 0], train_pairs[:, 1]], train_y)
+ 
+        if it % 100 == 0:
+            print('iteration:',it,'loss:',train_loss,'accuracy:',train_accuracy)
+        if (it+1) % 1000 == 0:
+            result = []
+            for i in range(len(x_test)):
+                test_pairs=[]
+#                if i%1000 == 0:
+#                    print(i)
+                for j in range(num_classes):
+        #            a = x_test[digit_indices[0][0]]
+                    a = x_test[i]
+                    b = x_test[test_digit_indices[j][10]]
+                    test_pairs += [[a,b]]
+                test_pairs = np.array(test_pairs)
+                
+                result += [model.predict([test_pairs[:, 0], test_pairs[:, 1]])]
+                
+            result = np.array(result)[:,:,0]
+            pre = np.argmin(result,axis=1)
+            acc = np.mean(pre==y_test)
+            print('test accuracy:',acc)
+            
+        start += 1
+    path = './model/model_'+str(start)+'.h5'
+    model.save(path)
+    print('model save:',path)
+
+elif train == False:    
     model = load_model('./model/model.h5',custom_objects={'contrastive_loss': contrastive_loss,'accuracy':accuracy})
 #    y_pred = model.predict([te_pairs[:, 0], te_pairs[:, 1]])
 #    te_acc = compute_accuracy(te_y, y_pred)
